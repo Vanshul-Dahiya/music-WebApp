85eb763ac684d898b6002df6ec9a4d53ee91399b
Vanshul <vanshuldahiya2002@gmail.com>
2023-01-27 11:55:22 +0530

    play each song functionality ,prev and next btn added

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+// ! Improvements to do ->
+// * switch songItemPlay to pause button when paused from masterPlay
+// * add responsiveness
+// * bottom div
+// * automatically play next song when previous song ends (songIndex++)
+
 // initialize the variables
-let songInd = 0;
+let songIndex = 0;
 const audioEl = new Audio("songs/1.mp3");
 // play button
 const masterPlay = document.getElementById("masterPlay");
 const progressBar = document.getElementById("progressBar");
 const gifEl = document.getElementById("gif");
+const masterSongNameEl = document.getElementById("masterSongName");
 const songItems = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
@@ -65,9 +72,8 @@ songItems.forEach((element, i) => {
   element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
 });
 
-// audioEl.play();
-
 // handle play/pause click
+
 masterPlay.addEventListener("click", () => {
   if (audioEl.paused || audioEl.currentTime <= 0) {
     audioEl.play();
@@ -82,7 +88,7 @@ masterPlay.addEventListener("click", () => {
   }
 });
 
-// listen to events
+// ! listen to events
 
 audioEl.addEventListener("timeupdate", () => {
   // update seekbar
@@ -92,4 +98,63 @@ audioEl.addEventListener("timeupdate", () => {
 
 progressBar.addEventListener("change", () => {
   audioEl.currentTime = (progressBar.value * audioEl.duration) / 100;
+});
+
+// ! change play button to pause button in songList
+
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+    (element) => {
+      element.classList.remove("fa-pause");
+      element.classList.add("fa-play");
+    }
+  );
+};
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      console.log(e.target);
+      makeAllPlays();
+      songIndex = parseInt(e.target.id);
+      e.target.classList.remove("fa-play");
+      e.target.classList.add("fa-pause");
+      audioEl.src = `songs/${songIndex}.mp3`;
+      masterSongNameEl.innerText = songs[songIndex].songName;
+      audioEl.currentTime = 0;
+      audioEl.play();
+      gifEl.style.opacity = 1;
+      masterPlay?.classList.remove("fa-play");
+      masterPlay?.classList.add("fa-pause");
+    });
+  }
+);
+
+document.getElementById("next").addEventListener("click", () => {
+  if (songIndex >= 10) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  audioEl.src = `songs/${songIndex}.mp3`;
+  masterSongNameEl.innerText = songs[songIndex].songName;
+  audioEl.currentTime = 0;
+  audioEl.play();
+  gifEl.style.opacity = 1;
+  masterPlay?.classList.remove("fa-play");
+  masterPlay?.classList.add("fa-pause");
+});
+document.getElementById("previous").addEventListener("click", () => {
+  if (songIndex <= 1) {
+    songIndex = 0;
+  } else {
+    songIndex -= 1;
+  }
+  audioEl.src = `songs/${songIndex}.mp3`;
+  masterSongNameEl.innerText = songs[songIndex].songName;
+  audioEl.currentTime = 0;
+  audioEl.play();
+  gifEl.style.opacity = 1;
+  masterPlay?.classList.remove("fa-play");
+  masterPlay?.classList.add("fa-pause");
 });
